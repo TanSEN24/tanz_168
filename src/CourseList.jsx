e1726e13aa0de7ccbbbecd94f65c89e4b090855a
TanSEN24 <sentan909@email.com>
2026-08-26 18:44:17 +0100

    First Commit react app

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import CourseCard from './CourseCard'
+import { courses } from './data/course'
 
 function CourseList() {
   return (
     <div className="courses">
-          <CourseCard title="Book1" desc="Decscription Book1" image="1.png" price="10"/>
-          <CourseCard title="Book2" desc="Decscription Book2" image="1.png" price="89"/>
-          <CourseCard title="Book3" desc="Decscription Book3" image="1.png" price="46"/>
-          <CourseCard title="Book3" desc="Decscription Book4" image="1.png" price="31"/>
+      {
+
+      }
+      
+      {
+        courses.map((course) =>(
+          <CourseCard
+          key={course.id}
+          title={course.title}
+          desc={course.description}
+          image={course.image_url}
+          price={course.price}
+          />
+        ))
+      }
     </div>
   )
 }
